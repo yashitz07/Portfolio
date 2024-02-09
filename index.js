0719aae120f8b539e9b97fd985064b2086698f3c
YashPatel <yashitz07@gmail.com>
2024-02-09 13:51:59 +0530

    deployment changes

--- index.js
+++ index.js
@@ -32,20 +32,20 @@ app.get("/achievement", (req, res) => {
 app.get("/projects", (req, res) => {
   res.render("projects.ejs");
 });
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "user_data",
-  password: "database@2604",
-  port: 5432,
-});
-db.connect()
-    .then(() => {
-        console.log('Connected to the database');
-    })
-    .catch((error) => {
-        console.error('Error connecting to the database', error);
-    });
+// const db = new pg.Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "user_data",
+//   password: "database@2604",
+//   port: 5432,
+// });
+// db.connect()
+//     .then(() => {
+//         console.log('Connected to the database');
+//     })
+//     .catch((error) => {
+//         console.error('Error connecting to the database', error);
+//     });
 app.post('/submit', (req, res) => {
     const { name, email, message } = req.body;
   
